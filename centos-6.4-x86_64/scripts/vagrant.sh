12035f62fa366b8416e4fa935cad27951a32084c
Stefan Freyr Stefansson <stefan.stefansson@decode.is>
2014-02-19 18:19:47 +0000

    added first packer template - centos 6.5

---
+++ centos-6.4-x86_64/scripts/vagrant.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+set -e
+set -x
+
+if [ "$(ping -q -c1 proxy.decode.is)" ] ; then 
+  echo "Using decode proxy server."
+  export http_proxy=http://proxy.decode.is:8080
+  export https_proxy=http://proxy.decode.is:8080
+else
+  echo "Not setting proxy server."
+fi
+
+sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
+sed -i "s/#UseDNS yes/UseDNS no/" /etc/ssh/sshd_config
+
+# Add the EPEL Repository
+/bin/cat << EOF > /etc/yum.repos.d/epel.repo
+[epel]
+name=Extra Packages for Enterprise Linux 6 - \$basearch
+baseurl=http://download.fedoraproject.org/pub/epel/6/\$basearch
+enabled=1
+gpgcheck=0
+EOF
+
+# Install vagrant keys
+/bin/mkdir /home/vagrant/.ssh
+/bin/chmod 700 /home/vagrant/.ssh
+/usr/bin/curl -o /home/vagrant/.ssh/id_rsa https://raw.github.com/mitchellh/vagrant/master/keys/vagrant
+/usr/bin/curl -o /home/vagrant/.ssh/authorized_keys https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub
+/bin/chown -R vagrant:vagrant /home/vagrant/.ssh
+/bin/chmod 0400 /home/vagrant/.ssh/*
+
+# Configure sudo
+/bin/cat << EOF > /etc/sudoers.d/wheel
+Defaults:%wheel env_keep += "SSH_AUTH_SOCK"
+Defaults:%wheel !requiretty
+%wheel ALL=NOPASSWD: ALL
+EOF
+/bin/chmod 0440 /etc/sudoers.d/wheel
+
+
+yum -y install git gcc make automake autoconf libtool gcc-c++ zlib-devel openssl-devel \
+               readline-devel sqlite-devel perl wget nfs-utils bind-utils dkms sudo \
+               rubygems ruby-devel kernel-headers-`uname -r` kernel-devel-`uname -r`
+
+echo `uname -r`
+
+# # Puppet and chef
+# /usr/bin/gem install --no-ri --no-rdoc puppet
+# /usr/sbin/groupadd -r puppet
+# /usr/bin/gem install --no-ri --no-rdoc chef
+
+if [[ -f /etc/.vbox_version ]]; then
+  cd /tmp
+  mount -o loop /tmp/VBoxGuestAdditions.iso /mnt
+  sh /mnt/VBoxLinuxAdditions.run || true
+  umount /mnt
+  #rm -rf /tmp/VBoxGuestAdditions*.iso
+
+  /etc/rc.d/init.d/vboxadd setup
+fi
